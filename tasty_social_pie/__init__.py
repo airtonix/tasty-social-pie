e6e0ab0035a17b224526c430c6fa1edc4bbb393d
Zenobius Jiricek <airtonix@gmail.com>
2013-12-27 15:43:34 +1030

    actually include the warning

--- tasty_social_pie/__init__.py
+++ tasty_social_pie/__init__.py
@@ -1,3 +1,3 @@
 __version__ = "0.0.1"
 import warnings
-warnings.warn("You've checked out the master branch of Tasty Social Pie. Unfortunately there's nothing here, you may want to look at the develop branch where bleeding edge action happens.")
+warnings.warn("You've checked out the master branch of Tasty Social Pie. Unfortunately there's nothing here, you may want to look at the develop branch where bleeding edge action happensPcom.")
